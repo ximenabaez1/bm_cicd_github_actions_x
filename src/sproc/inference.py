e5b5b6280f92743a2b01121ff6865692b9f1f548
erickAccenture <erickperezargueta2@gmail.com>
2024-05-16 19:52:47 -0600

    config inicial actions on dev; se realiza el setup un del ambiente de trabajo de sf

---
+++ src/sproc/inference.py
@@ -0,0 +1,22 @@
+from snowflake.snowpark.session import Session
+import os
+import json
+
+my_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+connection_parameters = json.load(open(os.path.join(my_dir, 'creds.json')))
+sess = Session.builder.configs(connection_parameters).create()
+
+def do_inference():
+    print("STORE PROCEDURE QUE REPRESENTA LA REALIZACION DE INFERENCIAS")
+
+
+sproc = sess.sproc.register(func=do_inference,
+                                  name='inference_step',
+                                  is_permanent=True,
+                                  replace=True,
+                                  stage_location='@PYTHON_FILES',
+                                  packages=['snowflake-ml-python',
+                                            'snowflake-snowpark-python'
+                                           ])
+
+
