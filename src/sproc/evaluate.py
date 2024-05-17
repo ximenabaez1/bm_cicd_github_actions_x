925e0aaf7c7b2243e4cb4c7e2e3a5c958afb8e0f
erickAccenture <erickperezargueta2@gmail.com>
2024-05-17 15:09:40 -0600

    se agrega escritura de SP

--- src/sproc/evaluate.py
+++ src/sproc/evaluate.py
@@ -27,17 +27,17 @@ session.use_database(dict_creds['database'])
 session.use_schema(dict_creds['schema'])
 
 print("SI SE USA DB Y SCHEMA")
-# def do_evaluation():
-#     print("STORE PROCEDURE QUE REPRESENTA EVALUACION DEL MODELO")
+def do_evaluation():
+    print("STORE PROCEDURE QUE REPRESENTA EVALUACION DEL MODELO")
 
 
-# sproc = session.sproc.register(func=do_evaluation,
-#                                   name='evaluation_step',
-#                                   is_permanent=True,
-#                                   replace=True,
-#                                   stage_location='@PYTHON_FILES',
-#                                   packages=['snowflake-ml-python',
-#                                             'snowflake-snowpark-python'
-#                                            ])
+sproc = session.sproc.register(func=do_evaluation,
+                                  name='evaluation_step',
+                                  is_permanent=True,
+                                  replace=True,
+                                  stage_location='@PYTHON_FILES',
+                                  packages=['snowflake-ml-python',
+                                            'snowflake-snowpark-python'
+                                           ])
 
 
