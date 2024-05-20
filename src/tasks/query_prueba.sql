a0077266468a409432517c7af69b1ed8adc184f7
erickAccenture <erickperezargueta2@gmail.com>
2024-05-19 22:12:29 -0600

    elimina codigo comentado de sql

--- src/tasks/query_prueba.sql
+++ src/tasks/query_prueba.sql
@@ -1,11 +1,3 @@
 USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE COMPUTE_WH;
-
 CREATE DATABASE mytestdb;
--- USE DATABASE $DB_NAME$;
--- USE SCHEMA $SCHEMA$;
-
--- CREATE OR REPLACE TABLE mi_tabla (
---                 id INT,
---                 nombre STRING
---             );
